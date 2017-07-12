b7e38624bba5307c4080c9e3439cd08e8bbc8380
Deyvid <deyvidmitkov@gmail.com>
2017-07-11 21:08:05 -0400

    made contact with the site, but the sum's wrong

--- src/getethos.php
+++ src/getethos.php
@@ -31,4 +31,50 @@ class GetEthos
 
 		return $this->generateCoolNumber($sum);
     }
+    
+    public function solve()
+    {
+        $filename = __DIR__."/sumTo1Mil";
+        //if (!file_exists($filename)) 
+        {
+            $sum = 0;
+            foreach(range(1,1000000) as $num)
+            {
+                if (($result = $this->generateCoolNumber($num)) === 1)
+                {
+                    $sum += $result;
+                }
+            }
+            file_put_contents(__DIR__."/sumTo1Mil", $sum);
+        }
+        //what is security? 
+        $sum = file_get_contents($filename);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "http://dev.getethos.com/code1",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "POST",
+          CURLOPT_HTTPHEADER => array(
+            "x-cool-sum: 3745182"
+          ),
+        ));
+
+        foreach (range(1, 100) as $num)
+        {
+            curl_setopt_array($curl, [
+                CURLOPT_URL => "http://dev.getethos.com/code$num",
+                CURLOPT_HTTPHEADER => [
+                    "x-cool-sum: $sum"
+                ]
+                ]);
+            $response = curl_exec($curl);
+            if ($response !== "Not Found") {
+                echo $response."\n";
+            }
+        }
+    }
 }
