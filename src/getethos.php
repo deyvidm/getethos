11457a2ecc3180da99a7de5a622db7f0c2666b9a
Deyvid <deyvidmitkov@gmail.com>
2017-07-14 20:27:23 -0400

    ripped apart some methods to make cheating easier

--- src/getethos.php
+++ src/getethos.php
@@ -2,21 +2,21 @@
 
 class GetEthos
 {
-	public function generateCoolNumber($base)
+    public function generateCoolNumber($base)
     {
-		try 
-		{
-			$clean = intval($base);
-			if ((string)$clean !== (string)$base)
-			{
-				return false;
-			}
-		}
-		catch(Exception $e)
-		{
-			return false;
-		}
-        
+        try 
+        {
+            $clean = intval($base);
+            if ((string)$clean !== (string)$base)
+            {
+                return false;
+            }
+        }
+        catch(Exception $e)
+        {
+            return false;
+        }
+
         if (strlen($base) == 1)
         {
             return $base;
@@ -29,52 +29,66 @@ class GetEthos
             $sum += $number*$number; 
         }
 
-		return $this->generateCoolNumber($sum);
+        return $this->generateCoolNumber($sum);
     }
-    
+
     public function solve()
     {
         $filename = __DIR__."/sumTo1Mil";
         //if (!file_exists($filename)) 
+    {
+        $sum = 0;
+        foreach(range(1,1000000) as $num)
         {
-            $sum = 0;
-            foreach(range(1,1000000) as $num)
+            if (($result = $this->generateCoolNumber($num)) === 1)
             {
-                if (($result = $this->generateCoolNumber($num)) === 1)
-                {
-                    $sum += $result;
-                }
+                $sum += $num;
             }
-            file_put_contents(__DIR__."/sumTo1Mil", $sum);
         }
+        file_put_contents(__DIR__."/sumTo1Mil", $sum);
+    }
         //what is security? 
         $sum = file_get_contents($filename);
-
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => "http://dev.getethos.com/code1",
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "POST",
-          CURLOPT_HTTPHEADER => array(
-            "x-cool-sum: 3745182"
-          ),
-        ));
+        $curl = $this->getCh(); 
 
         foreach (range(1, 100) as $num)
         {
-            curl_setopt_array($curl, [
-                CURLOPT_URL => "http://dev.getethos.com/code$num",
-                CURLOPT_HTTPHEADER => [
-                    "x-cool-sum: $sum"
-                ]
-                ]);
-            $response = curl_exec($curl);
+
+            $response = $this->makeRequest($curl, $num, $sum);
             if ($response !== "Not Found") {
                 echo $response."\n";
             }
         }
+    }
+
+    private function makeRequest($ch, $num, $sum)
+    {
+        curl_setopt_array($ch, [
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_URL => "http://dev.getethos.com/code$num",
+            CURLOPT_HTTPHEADER => [
+            "x-cool-sum: $sum"
+            ]
+        ]);
+        return curl_exec($ch);
+    }
+
+    public function cheat()
+    {
+    }
+
+    private function getCh()
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "http://dev.getethos.com/code1",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+        ));
+
+        return $curl;
     }
 }
